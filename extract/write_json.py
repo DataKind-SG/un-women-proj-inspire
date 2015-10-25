9e12d1ebf512c14d847f582861d5efdbe3c50925
oliverxchen <oliverxchen@gmail.com>
2015-10-25 15:40:04 +0800

    outputting json for 2011-2014 but country names not cleaned yet and impact countries are just the same as application countries

--- extract/write_json.py
+++ extract/write_json.py
@@ -22,10 +22,10 @@ def dict_factory(cursor, row):
 
 def get_year_data(year):
     y = (year,)
-    with sqlite3.connect('../source_data/early.db') as conn:
+    with sqlite3.connect('../source_data/un_women_data.sqlite') as conn:
         conn.row_factory = dict_factory
         c = conn.cursor()
-        c.execute('SELECT * FROM table WHERE project_year=?', y)
+        c.execute('SELECT * FROM un_women WHERE year=?', y)
         data = c.fetchall()
     return data
 
@@ -37,14 +37,34 @@ def get_test_data(year):
     return data
 
 
+def get_country_code(country_name, iso_map):
+    try:
+        country_code = iso_map[country_name]
+    except KeyError:
+        print('Unknown country name: {}'.format(country_name))
+        country_code = ''
+
+    return country_code
+
 def transform_data(input, iso_map):
     output = list()
     for row in input:
         out_row = dict()
-        out_row['project_year'] = row['project_year']
-        out_row['title'] = row['title']
-        out_row['country'] = row['country']
-        out_row['country_code'] = iso_map[row['country']]
+        out_row['project_details_other'] = row['other_details']
+        out_row['project_id'] = row['application_id']
+        out_row['project_summary'] = row['summary']
+        out_row['project_name'] = row['project_name']
+        out_row['sectors'] = ''
+
+        country_name = row['project_location_2'].strip()
+        country_code = get_country_code(country_name, iso_map)
+
+        out_row['country_application_name'] = country_name
+        out_row['country_impact_name'] = country_name # needs modification
+        out_row['country_application'] = country_code
+        out_row['country_impact'] = country_code # needs modification
+        out_row['project_details'] = row['project_details']
+        out_row['project_year'] = row['year']
         output.append(out_row)
 
     return output
@@ -58,8 +78,7 @@ def write_data(data, year):
 def main():
     iso_map = import_iso_map()
     for year in range(2011, 2015):
-        # raw_data = get_year_data(year)
-        raw_data = get_test_data(year)
+        raw_data = get_year_data(year)
 
         transformed_data = transform_data(raw_data, iso_map)
         write_data(transformed_data, year)

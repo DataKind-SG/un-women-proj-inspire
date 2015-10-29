ffdc024c3004cf93c265373c59b6ae0c6f1c29c4
Raymond Chan <raymond.chan.wai.mun@gmail.com>
2015-10-29 19:16:53 +0800

    Manual corrections of data with email applicants. As well as change name of R-visualisation to R-animation.

--- R-animation/vis.R
+++ R-animation/vis.R
@@ -6,22 +6,24 @@ library(htmlwidgets)
 country <- read.csv("data/Country_List_ISO_3166_Codes_Latitude_Longitude.csv")
 country_data <- fromJSON("data/impacted.json")
 
-data_2015 <- left_join(country_data$year_2011, country, c("code" = "Alpha.2.code"))
+data_2015 <- left_join(country_data$year_2015, country, c("code" = "Alpha.2.code"))
 
 s = 4
-n = 4
+n = 60
 start.lng <- 103.8
 start.lat <- 35
 start.zoom <- 3
 
-end.lng <- 159
-end.lat <- -8
+end.lng <- 133 #159
+end.lat <- -27 #-8
+end.zoom <- 5
 
 lng.delta <- (end.lng - start.lng)/n
 lat.delta <- (end.lat - start.lat)/n
+zoom.delta <- (end.zoom - start.zoom)/n
 
 m <- leaflet(width = floor(s*512), height=floor(s*265)) %>%
-  setView(lng=start.lng + l*lng.delta, lat=start.lat + l*lat.delta, zoom=start.zoom+l) %>%
+  setView(lng=start.lng, lat=start.lat, zoom=start.zoom) %>%
   addProviderTiles("Stamen.Toner") %>%
   addCircleMarkers(
     lng=data_2015$Longitude..average., 
@@ -29,15 +31,13 @@ m <- leaflet(width = floor(s*512), height=floor(s*265)) %>%
     radius=data_2015$z,
     popup=data_2015$Country)
 
+print(m)
 for (l in 0:n){
   m <- m %>%
-    setView(lng=start.lng + l*lng.delta, lat=start.lat + l*lat.delta, zoom=start.zoom+l)
+    setView(lng=start.lng + l*lng.delta, lat=start.lat + l*lat.delta, zoom=floor(start.zoom+l*zoom.delta))
   
   print(paste("shot", l, ".html", sep=""))
   Sys.sleep(1)
   print(m)
   saveWidget(m, paste("shot", l, ".html", sep=""))
 }
-
-
-#
